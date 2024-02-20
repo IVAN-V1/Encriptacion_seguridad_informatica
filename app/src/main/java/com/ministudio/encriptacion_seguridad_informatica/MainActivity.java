a158837266f06600779dd06ac8025e73a72bae7a
IVAN-V1 <cuentabim1@gmail.com>
2024-02-20 13:08:45 -0600

    template para iniciar ejemplos de encriptacion

---
+++ app/src/main/java/com/ministudio/encriptacion_seguridad_informatica/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ministudio.encriptacion_seguridad_informatica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
