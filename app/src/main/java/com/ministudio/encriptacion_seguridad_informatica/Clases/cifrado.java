8702fc0d0576cec584b48aa53b7bb21f55ba06c4
IVAN-V1 <cuentabim1@gmail.com>
2024-02-24 14:33:26 -0600

    Encriptacion UTF-8 TERMINADA (IVAN)

---
+++ app/src/main/java/com/ministudio/encriptacion_seguridad_informatica/Clases/cifrado.java
@@ -0,0 +1,39 @@
+package com.ministudio.encriptacion_seguridad_informatica.Clases;
+
+import android.widget.EditText;
+
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class cifrado {
+
+    public byte[] cifra(String sinCifrar) throws Exception {
+        final byte[] bytes = sinCifrar.getBytes("UTF-8");
+        final Cipher aes = obtieneCipher(true);
+        final byte[] cifrado = aes.doFinal(bytes);
+        return cifrado;
+    }
+
+    public String descifra(byte[] cifrado) throws Exception {
+        final Cipher aes = obtieneCipher(false);
+        final byte[] bytes = aes.doFinal(cifrado);
+        final String sinCifrar = new String(bytes, "UTF-8");
+        return sinCifrar;
+    }
+
+    private Cipher obtieneCipher(boolean paraCifrar) throws Exception {
+        final MessageDigest digest = MessageDigest.getInstance("SHA");
+        final SecretKeySpec key = new SecretKeySpec(digest.digest(), 0, 16, "AES");
+        final Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        if (paraCifrar) {
+            aes.init(Cipher.ENCRYPT_MODE, key);
+        } else {
+            aes.init(Cipher.DECRYPT_MODE, key);
+        }
+
+        return aes;
+    }
+
+}
--- app/src/main/java/com/ministudio/encriptacion_seguridad_informatica/MainActivity.java
+++ app/src/main/java/com/ministudio/encriptacion_seguridad_informatica/MainActivity.java
@@ -11,7 +11,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.ministudio.encriptacion_seguridad_informatica.Clases.UTF_8_encriptacion;
+import com.ministudio.encriptacion_seguridad_informatica.Clases.cifrado;
+
+import java.nio.charset.StandardCharsets;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,10 +27,6 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        EditText editText=findViewById(R.id.editext_texto_input);
-
-        String texto= editText.getText().toString();
 
 
 
@@ -50,22 +50,48 @@ public class MainActivity extends AppCompatActivity {
                 AutoCompleteTextView tipo_encriptacion = findViewById(R.id.Auto_completext);
                 String tipo_encrip = tipo_encriptacion.getText().toString();
 
+
+                EditText texto_p_cifrar=findViewById(R.id.editext_texto_input);
+
+                String texto= texto_p_cifrar.getText().toString();
+
                 EditText resultado = findViewById(R.id.resultado);
 
-                if (tipo_encrip.equals("")) {
-                    Toast.makeText(MainActivity.this, "Campo vacío", Toast.LENGTH_SHORT).show();
+                EditText text_no_cifrado = findViewById(R.id.texto_desifrado);
+
+
+                if (texto.equals("")) {
+
+                    // Obtén una referencia al TextInputLayout
+                    TextInputLayout input_text_normal = findViewById(R.id.textInputLayout_texto_normal);
+                    input_text_normal.setError(getString(R.string.error));
+                    // Obtén una referencia al TextInputLayout
+
+
+                } else if (tipo_encrip.equals("")) {
+
+                    TextInputLayout input_tipo_cifrado = findViewById(R.id.tipo_encriptacion);
+                    input_tipo_cifrado.setError(getString(R.string.error));
+
                 } else if (tipo_encrip.equals("MD5")) {
                     // Lógica para MD5
                 } else if (tipo_encrip.equals("SHA 256")) {
                     // Lógica para SHA 256
                 } else if (tipo_encrip.equals("UTF-8")) {
-                    // Limpiar el campo de resultado antes de mostrar el resultado de la nueva encriptación
-                    resultado.setText("");
 
-                    Toast.makeText(MainActivity.this, "UTF-8", Toast.LENGTH_SHORT).show();
 
-                    UTF_8_encriptacion utf8Encriptacion = new UTF_8_encriptacion();
-                    utf8Encriptacion.encryptAndSetEditText(texto, resultado);
+                    cifrado cifrado=new cifrado();
+                    try {
+
+                        resultado.setText(cifrado.cifra(texto).toString());
+
+                        byte[] des=cifrado.cifra(texto);
+                        text_no_cifrado.setText(cifrado.descifra(des));
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
                 }
             }
         });
